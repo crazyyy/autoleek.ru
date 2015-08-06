5bfbaa3d9aeca806581b68ef444d8dabeeadb6ae
crazyyy <crazyyy@gmail.com>
2015-08-06 04:38:38 +0300

    bf cat wid

--- wordpress/wp-content/themes/material/searchform.php
+++ wordpress/wp-content/themes/material/searchform.php
@@ -1,6 +1,6 @@
 <div class="search-box">
   <form role="search" method="get" id="searchform" action="<?php echo home_url() ; ?>/">
     <input type="text" value="<?php echo esc_html($s, 1); ?>" name="s" id="s" maxlength="33" class="form-control pull-left" placeholder="Поиск" />
-    <input type="submit" id="searchsubmit" value="Поиск" class="btn">
+    <button type="submit" class="fa" id="searchsubmit"></button>
   </form>
 </div>
